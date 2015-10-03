e11888c1c1663a1ebcb81069ebc3f2defc92ec42
Martin Schinz <m.schinz@gmail.com>
2015-10-03 18:16:24 +0100

    error handling

--- apps/wfh2/src/wfh2_worker_handler.erl
+++ apps/wfh2/src/wfh2_worker_handler.erl
@@ -20,7 +20,7 @@ rest_init(Req, _State) ->
   {ok, Req, #rest_state{}}.
 
 allowed_methods(Req, State) ->
-  {[<<"GET">>, <<"PUT">>], Req, State}.
+  {[<<"GET">>, <<"PUT">>, <<"POST">>], Req, State}.
 
 content_types_accepted(Req, State) ->
   {[{{<<"application">>, <<"json">>, []}, put_json}],
@@ -61,28 +61,30 @@ put_json(Req, State) ->
 
         <<"location">> ->
           {ok, BodyRaw, Req3} = cowboy_req:body(Req2),
-          BodyDes = jsx:decode(BodyRaw, [return_maps, {labels, atom}]),
-
-          case maps:get(location, BodyDes) of
-
-            {badkey, _} ->
-              {false, Req3, State};
-
-            Location    ->
-              error_logger:info_msg("Location: ~p~n", [Location]),
-              case Location of
-                <<"InOffice">>     -> wfh2_worker:set_wfo(Id);
-                <<"OutOfOffice">>  ->
-                  Info = binary_to_list(maps:get(details, BodyDes, <<"">>)),
-                  wfh2_worker:set_wfh(Id, Info)
-              end,
-
-              {true, Req3, State}
+          try jsx:decode(BodyRaw, [return_maps, {labels, atom}]) of
+            BodyDes -> try maps:get(location, BodyDes) of
+                         Location -> error_logger:info_msg("Location: ~p~n", [Location]),
+                                     case Location of
+                                       <<"InOffice">>     -> wfh2_worker:set_wfo(Id);
+                                       <<"OutOfOffice">>  ->
+                                         Info = binary_to_list(maps:get(details, BodyDes, <<"">>)),
+                                         wfh2_worker:set_wfh(Id, Info)
+                                     end,
+                                     {true, Req3, State}
+                       catch
+                         error:{badkey, location } -> Body = jsx:encode(#{error => <<"location field missing">>}),
+                                         Req4 = cowboy_req:set_resp_body(Body, Req3),
+                                         {false, Req4, State}
+                       end
+          catch
+            error:badarg -> Body = jsx:encode(#{error => <<"request body could not be read">>}),
+                            Req4 = cowboy_req:set_resp_body(Body, Req3),
+                            {false, Req4, State}
           end;
-        _ ->
-          Body = jsx:encode(#{ error => <<"Unsupported action">> }),
-          Req3 = cowboy_req:set_resp_body(Body, Req2),
-          { false, Req3, State }
+
+        _ -> Body = jsx:encode(#{ error => <<"Unsupported action">> }),
+             Req3 = cowboy_req:set_resp_body(Body, Req2),
+             { false, Req3, State }
       end
   end.
 
